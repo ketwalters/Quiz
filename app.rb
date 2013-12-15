c25b5d4506b6eccc0d8d60cd157a981cae06b8ea
Keturah Walters <your.ketwalters@gmail.com>
2013-12-15 00:45:55 -0500

    Add jayz kanye quiz

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'bundler'
+Bundler.require
+
+class Quiz < Sinatra::Application
+
+  get '/quiz' do
+    erb :quiz
+  end
+
+  post '/quiz' do
+    array = []
+    answers = params.each do |key, value|
+      value.each do |k,v|
+        array << v
+        end
+      end
+      if array.count('jayz') >=2
+        @answer = "You are Jay Z"
+      end
+      if array.count('kanye') >=2 
+        @answer1 = "You are Kanye"
+    end
+    erb :results
+    end
+  end
